1341431901ff969d9b2a7653bed15aececb57d6c
pjw <1571933050@qq.com>
2021-09-15 17:26:41 +0800

    青蛙跳出井

--- RandCall.py
+++ RandCall.py
@@ -1,12 +1,12 @@
 # 输入编号，进行点名
-list = ["西园寺世界", "艾斯德斯", "夜刀神十香","时崎狂三","五河琴里","四系乃",";雷姆","2B"]
+nameList = ["西园寺世界", "艾斯德斯", "夜刀神十香", "时崎狂三", "五河琴里", "四系乃", ";雷姆", "2B"]
 while True:
     try:
         num = int(input("请输入点名的人的编号："))
-        if num < 0 or num >= len(list):
-            print("请输入0到{0}之间的数".format(len(list) - 1))
-        if num < len(list):
-            print(list[num])
+        if num < 0 or num >= len(nameList):
+            print("请输入0到{0}之间的数".format(len(nameList) - 1))
+        if num < len(nameList):
+            print(nameList[num])
     except:
         print("请输入一个正整数")
         continue
