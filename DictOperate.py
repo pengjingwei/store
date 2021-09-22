ea089dbd7cf374a5c465a003399dbd9655378f8d
pjw <1571933050@qq.com>
2021-09-22 14:52:38 +0800

    列出字典的键、值和增加键值对

---
+++ DictOperate.py
@@ -0,0 +1,9 @@
+# dict = {"k1":"v1","k2":"v2","k3":"v3"}
+dict = {"k1":"v1","k2":"v2","k3":"v3"}
+# 1、请循环遍历出所有的key
+print(dict.keys())
+# 2、请循环遍历出所有的value
+print(dict.values())
+# 3、请在字典中增加一个键值对,"k4":"v4"
+dict["k4"] = "v4"
+print(dict.items())
