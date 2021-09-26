0eebd763dbcb08d57ed4156fef7b26eca831286e
pjw <1571933050@qq.com>
2021-09-26 21:24:53 +0800

    打印*阵

--- PrintStart.py
+++ PrintStart.py
@@ -4,10 +4,10 @@ for i in range(7):
     j = 50-i
     t = i+1
     while j > 0:
-        print("",end=" ") # 每行打印7-i个空格
+        print("", end=" ") # 每行打印j个空格
         j -= 1
-    while t>0:
-        print("*",end=" ")  # 每行打印i+1个*
+    while t > 0:
+        print("*", end=" ")  # 每行打印i+1个*
         t -= 1
     print("")  # 换行
 
