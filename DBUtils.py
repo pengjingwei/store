eea7c80360258be58d188c95b9e78d180a5f791c
pjw <1571933050@qq.com>
2021-09-26 21:24:31 +0800

    连接数据库使用的工具类

---
+++ DBUtils.py
@@ -0,0 +1,34 @@
+# 操作数据库工具类
+import pymysql
+
+host = "localhost"
+user = "root"
+password = "pjw9285851"
+database = "bank"
+
+
+def updata(sql, param):
+    con = pymysql.Connect(host=host, user=user, password=password, database=database)
+    cursor = con.cursor()
+    cursor.execute(sql, param)
+
+    con.commit()
+    cursor.close()
+    con.close()
+
+
+def select(sql, param, model="all", size=1):
+    con = pymysql.Connect(host=host, user=user, password=password, database=database)
+    cursor = con.cursor()
+    cursor.execute(sql, param)
+
+    if model == "all":
+        return cursor.fetchall()
+    elif model == "one":
+        return cursor.fetchone()
+    elif model == "many":
+        return cursor.fetchmany(size)
+
+    con.commit()
+    cursor.close()
+    con.close()
