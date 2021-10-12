df2ad0d804426a1613c69ef1039ba16711672073
pjw <1571933050@qq.com>
2021-10-12 09:15:30 +0800

    计算器类

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# 计算器功能
+
+class Calc():
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def dev(self, a, b):
+        return a / b
