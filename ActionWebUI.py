4348e81a714df79d99079252088a4e8219b728de
pjw <1571933050@qq.com>
2021-10-18 09:51:59 +0800

    webui操作滑动验证条

--- ActionWebUI.py
+++ ActionWebUI.py
@@ -1,4 +1,5 @@
 # 滑动条验证
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -7,7 +8,15 @@ driver = webdriver.Chrome()
 driver.get(r'C:\Users\gamer\Desktop\python学习资料\htmls\mousedrag.html')
 
 ele = driver.find_element('xpath', '//*[@id="box"]/div[3]')
-
+# 创建事件链
 ac = ActionChains(driver)
-
-ac.move_to_element_with_offset(ele, 248, 0).perform()
+# 将鼠标移动到要移动的元素上面
+ac.move_to_element(ele)
+# 点击元素并保持
+ac.click_and_hold().perform()
+# 移动一直点击元素
+ac.move_to_element_with_offset(ele, 240, 0).perform()
+# 释放点击
+ac.release(ele)
+sleep(5)
+driver.quit()
